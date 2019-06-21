9a69b6de8a886f1179e9d0980c0c0a4fd6221b58
Charlie Lin <39326385+chickenlin@users.noreply.github.com>
2019-06-21 21:16:02 +0800

    Add files via upload

---
+++ C_MM10.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float x;
+    while (cin >> x)
+        cout << fixed << setprecision(1) << x * 9 / 5 + 32<< endl;
+    return 0;
+}
